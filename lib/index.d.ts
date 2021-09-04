e042361752dabad65ef3d31d0adba5c030be53f8
CrispenGari <Crispengari@gmail.com>
2021-09-05 01:06:08 +0200

    version:1.0.3

---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+export declare const generateInteger: () => Number;
+export declare const generateVerificationCode: (numberOfDigits?: number, allowZeroStart?: boolean, returnAsString?: boolean) => Promise<string | number>;
+//# sourceMappingURL=index.d.ts.map
